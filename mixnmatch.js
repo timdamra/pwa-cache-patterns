8de672e5d26ff16b603b32fbeaa156aeb2be6ce4
timdamra <hdamra@gmail.com>
2018-02-27 21:33:43 -0600

    sec commit

--- mixnmatch.js
+++ mixnmatch.js
@@ -18,4 +18,41 @@ self.addEventListener("fetch", event => {
 });
 
 // cache, then fall back to network, with frequent updates
+// resources that do change from time to time, but where showing the latest version is less important than returning a fast response 
 
+self.addEventListener("fetch", event => {
+    event.respondWith(
+        caches.open('cache-name')
+        .then(cache => {
+            return cache.match(event.request);
+        })
+        .then(cachedResponse => {
+            let fetchPromise = fetch(event.request)
+            .then(networkResponse => {
+                cache.put(event.request, networkResponse.clone());
+                return networkResponse;
+            })
+            return cachedResponse || fetchPromise;
+        })
+    )
+});
+
+// network first, falling back to cache, with frequent updates
+// every time the network is accessed successfully, it updates the cache with the network response
+// network response is then cached as backup for future requests
+
+self.addEventListener("fetch", (event) => {
+    event.respondWith(
+        caches.open("cache-name")
+        .then(cache => {
+            return fetch(event.request)
+                    .then((networkResponse) => {
+                        cache.put(event.request, networkResponse.clone());
+                        return networkResponse;
+                    })
+                    .catch(() => {
+                        return caches.match(event.request);
+                    });
+        })
+    );
+});
